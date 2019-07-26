6a3da1cbd6f6f4db6eb6beb49684458bfb5375d7
duanfapiaopiao <31460242+duanfapiaopiao@users.noreply.github.com>
2019-07-26 15:28:25 +0800

    Add files via upload

---
+++ src/servlet/HeroAddServlet.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import bean.Hero;
+import dao.HeroDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HeroAddServlet extends HttpServlet {
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        int hp  = Integer.parseInt(request.getParameter("hp"));
+        Hero hero = new Hero();
+        hero.setName(name);
+        hero.setHp(hp);
+        new HeroDAO().add(hero);
+
+        response.sendRedirect("listHero");
+
+    }
+}
